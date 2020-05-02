1a8ff5ff67d3d46bd21ed69897f1455b40801655
Arfarin <engelmayer@stud.dhbw-ravensburg.de>
2020-05-02 18:42:57 +0200

    commit

--- ZuulBad/Food.java
+++ ZuulBad/Food.java
@@ -1,5 +1,35 @@
 package ZuulBad;
 
-public class Food extends Item{
 
-}
+	import java.util.HashMap;
+
+	class Food extends Items{
+		int weight = 5;
+		private String name;
+		private HashMap<String, String> descriptions;
+		
+		public Food(String name) {
+			this.name = name;
+			descriptions = new HashMap<>();
+			this.fillDescriptions();
+		}
+		
+		private final void fillDescriptions() {
+			descriptions.put("apple", "This is a yummy apple");
+			descriptions.put("banana", "This is a yummy banana");
+			descriptions.put("starfruit", "This is a yummy starfruit");
+		}
+		
+		public String getDescription() {
+			return descriptions.get(name);
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public int getWeight() {
+			return weight;
+		}
+	}
+
