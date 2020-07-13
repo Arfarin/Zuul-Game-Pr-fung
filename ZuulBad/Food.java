71891d603e8577869679834f431d587b9cf78c59
kmatysova2 <kmatysova2@gmail.com>
2020-07-13 21:18:48 +0200

    javadoc comments added

--- ZuulBad/Food.java
+++ ZuulBad/Food.java
@@ -1,7 +1,7 @@
 package ZuulBad;
 
 /**
- * This class is responsible for food items in the game.
+ * This class is responsible for food items in the game
  * 
  * @author Katerina Matsyova
  *
@@ -9,13 +9,25 @@ package ZuulBad;
 
 public class Food extends Item implements Transportable {
 
+	/**
+	 * Constructor for the class Food
+	 * 
+	 * @param name item name
+	 * @param weight item weight
+	 * @param description item description
+	 */
 	public Food(String name, int weight, String description) {
 		super(name, weight, description);
 	}
-	
+
+	/**
+	 * Returns weight of the food item
+	 * 
+	 * @return weight
+	 */
 	@Override
-	public String getDescriptionWithWeight(){
+	public String getDescriptionWithWeight() {
 		return super.getDescription() + " " + " Weight: " + getWeight();
 	}
-	
+
 }
