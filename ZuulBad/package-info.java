b6c1481b18c71220859afb8643bb6878169884dd
Arfarin <Engelmayer@stud.dhbw-ravensburg.de>
2020-10-08 20:53:43 +0200

    hi

--- ZuulBad/package-info.java
+++ ZuulBad/package-info.java
@@ -5,7 +5,7 @@
  * 
  * Zuulbad is an adventure game for one single player. The player can move from room to room in a castle and must do certain things to survive and win the game. There are e.g. classes and functions to make possible that the player can eat food, collect different items in a backpack, get hints from other creatures in rooms and beat monsters with weapons.  
  * 
- * 
+ * hi
  * 
  * @author Katerina Matysova 
  * @author Daniel Birk
