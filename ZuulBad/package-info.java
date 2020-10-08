5eb6071c72379015b792425df6179558464f0770
Arfarin <Engelmayer@stud.dhbw-ravensburg.de>
2020-10-08 20:43:31 +0200

    single

--- ZuulBad/package-info.java
+++ ZuulBad/package-info.java
@@ -3,7 +3,7 @@
 /**
  * This package provides the classes necessary to create the game 'ZuulBad'.
  * 
- * Zuulbad is an adventure game for one player. The player can move from room to room in a castle and must do certain things to survive and win the game. There are e.g. classes and functions to make possible that the player can eat food, collect different items in a backpack, get hints from other creatures in rooms and beat monsters with weapons.  
+ * Zuulbad is an adventure game for one single player. The player can move from room to room in a castle and must do certain things to survive and win the game. There are e.g. classes and functions to make possible that the player can eat food, collect different items in a backpack, get hints from other creatures in rooms and beat monsters with weapons.  
  * 
  * 
  * 
